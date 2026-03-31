dd415b41be75a5067c012d789d955abce0b69402
Mahalakshmi <mahalakshmi2052005@gmail.com>
2026-03-31 15:00:00 +0530

    fix cors

--- server.js
+++ server.js
@@ -8,19 +8,14 @@ const expenseRoutes = require('./routes/expenses');
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/expenses', expenseRoutes);
 
 app.get('/', (req, res) => res.json({ message: 'Expense Tracker API Running' }));
 
-// MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('MongoDB connected');
